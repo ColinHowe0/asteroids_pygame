73cc3532e8dc228992120e7b26f28f5bfab016a3
ColinHowe0 <nilocewoh@gmail.com>
2025-02-21 04:18:36 -0800

    clock tick

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
             
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
